607eaf2fc18b06785274698b617b2110b1a984cd
Thotoo <Thobias.blankespoor@gmail.com>
2025-11-28 11:55:18 +0100

    changes

--- load_balancer.lua
+++ load_balancer.lua
@@ -10,7 +10,7 @@ local function read_modbus(id)
     local output = handle:read("*a")
     handle:close()
     local data = json.parse(output)
-    if data and data.values[1] then
+    if data and data.values and data.values[1] then
         return tonumber(data.values[1])
     else
         return nil
@@ -26,27 +26,28 @@ local function set_modbus(id, value)
     handle:close()
 end
 
-power_received = read_modbus(2.5)
-accu_state = read_modbus(7.9)
-soc = read_modbus(7.8)
-
-if power_received >= 10000 then
+local power_received = read_modbus(2.5)
+local power_delivered = read_modbus(2.6)
+local accu_state = read_modbus(7.9)
+local soc = read_modbus(7.8)
+if power_received and power_received >= 10000 then
     print("Waarschuwing: onrealistische waarden gedetecteerd. Gegevens niet verwerkt.")
     os.exit(1)
 end
 
-if power_received > 4000 then -- als de threshold overschreden wordt
-    set_tag_value("7.10", "2")
+if power_received and power_received > 4000 then -- als de threshold overschreden wordt
+    set_modbus("7.10", "2")
 else
-    if accu_state ~= 1 then -- als de accu niet al aan laden is
+    if accu_state and accu_state ~= 1 then -- als de accu niet al aan laden is
         set_modbus("7.10", "1")
     end
 
-    if soc >= 100 then -- als de accu vol is
+    if soc and soc >= 100 then -- als de accu vol is
         set_modbus("7.10", "0")
     end
 end
-
+local formatted = string.format("Power received: %d W | Power delivered: %d W", power_received, power_delivered)
+print(formatted)
 -- get all tags:
 -- ubus call modbus_client.rpc get_tags
 
